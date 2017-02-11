896b9d38b6a68a6f3174af4bad5de25d2e6fb26c
[Michael] <michael.burns@civica.co.uk>
2017-02-11 13:57:53 +0000

    Adding a basic notes controller and basic db context

---
+++ SimpleNote/Controllers/MyNotesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SimpleNote.Models;
+
+namespace SimpleNote.Controllers
+{
+    public class MyNotesController : Controller
+    {
+        private SimpleNoteContext db = new SimpleNoteContext();
+
+        // GET: MyNotes
+        public ActionResult Index()
+        {
+            return View(db.Notes.ToList());
+        }
+
+        // GET: MyNotes/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // GET: MyNotes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MyNotes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Body,CreatedDate,UpdatedDate,UserId")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                note.Id = Guid.NewGuid();
+                db.Notes.Add(note);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(note);
+        }
+
+        // GET: MyNotes/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // POST: MyNotes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Body,CreatedDate,UpdatedDate,UserId")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(note).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(note);
+        }
+
+        // GET: MyNotes/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // POST: MyNotes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Note note = db.Notes.Find(id);
+            db.Notes.Remove(note);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
