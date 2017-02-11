2475b3336bf3c3e56448cb2f756fb5f27bf74f7d
[Michael] <michael.burns@civica.co.uk>
2017-02-11 13:41:14 +0000

    Adding note model

---
+++ SimpleNote/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleNote.Models
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        // relationships 
+        // each note has a user Id
+        public Guid UserId { get; set; }
+    }
+}
